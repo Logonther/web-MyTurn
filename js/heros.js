42c6f665d70d410b71d419e18dd14f66190185ce
Logonther <736072727@qq.com>
2019-09-14 17:00:14 +0800

    按稀有度排列

--- js/heros.js
+++ js/heros.js
@@ -35,6 +35,9 @@ $(function () {
     $("#byType").click(function () {
         byType()
     });
+    $("#byRare").click(function () {
+        byRare()
+    });
 
     function byCost() {
         $.ajax({
@@ -87,6 +90,46 @@ $(function () {
                     if(hero1.type < hero2.type){
                         return -1;
                     }else if(hero1.type > hero2.type){
+                        return 1;
+                    }else{
+                        return 0;
+                    };
+                });
+                $.each(data,function(i,obj) {
+                    $(".heros").append("<div class=\"hero col-sm-3 col-xs-6\">\n" +
+                        "        <div class=\"content "+obj.type+' '+obj.rare+"\">\n" +
+                        "            <div class=\"title\">\n" +
+                        "                <span>"+obj.cost+"</span>" +obj.name+
+                        "            </div>\n" +
+                        "            <div class=\"pic\" style='background-image: url("+obj.pic+")'></div>\n" +
+                        "            <div class=\"skills\">\n" +
+                        "                <div class=\"skill\">Ex "+obj.ex[0].name+"：<br>"+obj.ex[0].txt+"</div>\n" +
+                        "                <div class=\"skill\">技能 "+obj.skill1[0].cost+"费 "+obj.skill1[0].name+"：<br>"+obj.skill1[0].txt+"</div>\n" +
+                        "                <div class=\"skill\">技能 "+obj.skill2[0].cost+"费 "+obj.skill2[0].name+"：<br>"+obj.skill2[0].txt+"</div>\n" +
+                        "                <div class=\"skill\">技能 "+obj.skill3[0].cost+"费 "+obj.skill3[0].name+"：<br>"+obj.skill3[0].txt+"</div>\n" +
+                        "            </div>\n" +
+                        "        </div>\n" +
+                        "    </div>")
+                    if (obj.derivative != "") {
+                        $.each(obj.derivative,function (j,o) {
+                            $(".hero .skills").eq(i).append("<div class=\"skill\">衍生 "+o.cost+"费 "+o.name+"：<br>"+o.txt+"</div>")
+                        })
+                    }
+                });
+            }
+        })
+    }
+    function byRare() {
+        $.ajax({
+            url:"js/heros.json",
+            datatype:"json",
+            type:"GET",
+            success:function(data){
+                $(".heros").empty();
+                data.sort(function(hero1, hero2) {
+                    if(hero1.rare < hero2.rare){
+                        return -1;
+                    }else if(hero1.rare > hero2.rare){
                         return 1;
                     }else{
                         return 0;
