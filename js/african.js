c3bf5167994ee4da4dc377c5f605bdadd2efbd77
Logonther <736072727@qq.com>
2019-09-15 16:36:20 +0800

    模拟抽卡

--- js/african.js
+++ js/african.js
@@ -1,10 +1,14 @@
 $(function () {
     $("#draw1").click(function () {
+        $(".cards").empty();
         draw1()
     });
     $("#draw5").click(function () {
+        $(".cards").empty();
         draw5()
     });
+    console.log($(".card").width());
+    $(".card").css("height",$(".card").width()*1.5);
 
     var african = [];
     var european = [];
@@ -18,26 +22,34 @@ $(function () {
             card = african[rollcard(african)];
             if (card.rare == "copper") {
                 console.log("不暴击"+card.name+"*3");
+                inin(card,card.name+"×3");
             }else{
                 console.log("不暴击"+card.name+"*1");
+                inin(card,card.name+"×1");
             }
         }else if (roll_no > 75 && roll_no <= 95) {
             card = european[rollcard(european)]
             if (card.rare == "copper") {
                 console.log("小暴击"+card.name+"*12");
+                inin(card,card.name+"×12");
             }else if (card.rare == "silver") {
                 console.log("小暴击"+card.name+"*4");
+                inin(card,card.name+"×4");
             }else{
                 console.log("小暴击"+card.name+"*1");
+                inin(card,card.name+"×1");
             }
         }else{
             card = european[rollcard(european)]
             if (card.rare == "copper") {
                 console.log("大暴击"+card.name+"*36");
+                inin(card,card.name+"×36");
             }else if (card.rare == "silver") {
                 console.log("大暴击"+card.name+"*12");
+                inin(card,card.name+"×12");
             }else{
                 console.log("大暴击"+card.name+"*3");
+                inin(card,card.name+"×3");
             }
         }
         return roll_no;
@@ -60,22 +72,49 @@ $(function () {
                 card = european[rollcard(european)]
                 if (card.rare == "copper") {
                     console.log("小暴击"+card.name+"*12");
+                    inin(card);
                 }else if (card.rare == "silver") {
                     console.log("小暴击"+card.name+"*4");
+                    inin(card);
                 }else{
                     console.log("小暴击"+card.name+"*1");
+                    inin(card);
                 }
             }else{
                 card = european[rollcard(european)]
                 if (card.rare == "copper") {
                     console.log("大暴击"+card.name+"*36");
+                    inin(card);
                 }else if (card.rare == "silver") {
                     console.log("大暴击"+card.name+"*12");
+                    inin(card);
                 }else{
                     console.log("大暴击"+card.name+"*3");
+                    inin(card);
                 }
             }
         }
+    }
+
+    function inin(card,txt) {
+        $(".cards").append("<div class=\"card col-sm-2 col-xs-6\">\n" +
+            "        <img src='image/cardback.jpg'>\n" +
+            "        <div class=\"cardfront\" style='background-image: url("+card.pic+")'>\n" +
+            "            <div class=\"title "+card.type+"\">\n" +
+            "                <span>"+card.cost+"</span>"+card.name+"\n" +
+            "            </div>\n" +
+            "        </div>\n" +
+            "        <div class='txt'><div class='rare "+card.rare+"'></div>"+txt+"</div>\n" +
+            "    </div>")
+        $(".card").css("height",$(".card").width()*1.5).click(function () {
+            $(this).find('img').css("transform","rotateY(180deg)");
+            $(this).find('.cardfront').css("transform","rotateY(360deg)");
+            var $that = $(this)
+            setTimeout(function () {
+                $that.find('.txt').css("opacity","1");
+            },500)
+
+        }).find(".txt").css("margin-top",$(".card").width()*1.5);
     }
 
     function rollcard(cards) {
